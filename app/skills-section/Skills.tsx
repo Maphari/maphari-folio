e4c606f36cb0eea2edbc04b0be31f44a54d1358c
Maphari <phumudzomaphari57@gmail.com>
2023-08-29 22:52:33 +0200

    updated

--- app/skills-section/Skills.tsx
+++ app/skills-section/Skills.tsx
@@ -116,7 +116,7 @@ export const Skills: React.FC = () => {
           whileInView={{ opacity: 1, x: 0 }}
           transition={transtition}
         >
-          C++
+          Python
         </motion.div>
         <motion.div
           className="bg-[#1F2937] py-3 px-4 rounded-xl"
