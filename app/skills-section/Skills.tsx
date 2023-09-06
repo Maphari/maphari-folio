ff889d84633dcfddaada4abe0da07e079476ef21
Phumudzo Maphari <104123979+Maphari@users.noreply.github.com>
2023-09-06 19:56:45 +0200

    Update Skills.tsx

--- app/skills-section/Skills.tsx
+++ app/skills-section/Skills.tsx
@@ -69,14 +69,6 @@ export const Skills: React.FC = () => {
       >
         Java | JavaFX
       </motion.div>
-      <motion.div
-          className="bg-[#1F2937] py-3 px-4 rounded-xl"
-          initial={{ opacity: 0, x: -30 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={transtition}
-        >
-          C# | ASP .NET | MAUI 
-        </motion.div>
         <motion.div
           className="bg-[#1F2937] py-3 px-4 rounded-xl"
           initial={{ opacity: 0, x: -30 }}
