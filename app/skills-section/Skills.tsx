28a9ae6f4f6a4fe8e1f92397a0f223407d5508de
Phumudzo Maphari <104123979+Maphari@users.noreply.github.com>
2023-09-16 07:02:12 +0200

    updated

--- app/skills-section/Skills.tsx
+++ app/skills-section/Skills.tsx
@@ -51,7 +51,7 @@ export const Skills: React.FC = () => {
           whileInView={{ opacity: 1, x: 0 }}
           transition={transtition}
         >
-          C#
+          Java
         </motion.div>
         <motion.div
           className="bg-[#1F2937] py-3 px-4 rounded-xl"
@@ -68,14 +68,6 @@ export const Skills: React.FC = () => {
           transition={transtition}
         >
           React Native
-        </motion.div>
-        <motion.div
-          className="bg-[#1F2937] py-3 px-4 rounded-xl"
-          initial={{ opacity: 0, x: -30 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={transtition}
-        >
-          .NET MAUI
         </motion.div>
         <motion.div
           className="bg-[#1F2937] py-3 px-4 rounded-xl"
