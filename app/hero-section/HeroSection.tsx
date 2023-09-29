870ef9576d11ab1144142ab4f5772907d843785a
Phumudzo Maphari <104123979+Maphari@users.noreply.github.com>
2023-09-29 21:16:35 +0200

    update

--- app/hero-section/HeroSection.tsx
+++ app/hero-section/HeroSection.tsx
@@ -65,7 +65,7 @@ export const HeroSection: React.FC = () => {
 
       <motion.div className="btn-container mt-12 mx-auto flex items-center gap-2 flex-wrap">
         <Link
-          href="https://drive.google.com/file/d/1tuIShgrt8u-pQUAM1gUDY8dIcStjWuCh/view?usp=drivesdk"
+          href="https://drive.google.com/file/d/11tqSDEw-uaUhDeu7WSPRrFcZiCMsryAL/view?usp=sharing"
           target="_blank"
           download="Maphari phumudzo resume.pdf"
           className="flex items-center gap-2 px-1"
